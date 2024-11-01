454108babcc13c498b6b66c2bdb82cabf449c4eb
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-11-01 23:43:53 +0530

    Add object literals for students and Twitter post

---
+++ Javascript/objectLiterals.js
@@ -0,0 +1,5 @@
+const students = {
+    name: "Rohan",
+    age: 21
+};
+console.log(students);
---
+++ Twitter Post/script.js
@@ -0,0 +1,8 @@
+const post ={
+    username : "mayurnikumbh",
+    content : "day one on NNN",
+    likes : 100,
+    tags : ["#newbie", "#dayone", "#NNN"]
+
+};
+console.log(post);
