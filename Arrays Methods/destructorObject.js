dae1fef875e2b56b863d8348fbf7b62d7566384c
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-11-08 22:52:24 +0530

    Add examples of object destructuring with account details

---
+++ Arrays Methods/destructorObject.js
@@ -0,0 +1,10 @@
+let account = {
+    username: "Mayur",
+    accountNumber: 123456,
+    balance: 100000,
+    password: "123456"
+};
+
+let { username, password } = account;
+
+console.log(username, password);
