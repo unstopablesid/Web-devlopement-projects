3c3709b26d90812c94830335aca1332f8bd04322
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-11-15 23:51:31 +0530

    Add basic text editor functionality with input display

---
+++ DOM/TextEditor/script.js
@@ -0,0 +1,7 @@
+let inp = document.querySelector("#text");
+let p = document.querySelector("p");
+
+inp.addEventListener("input", function() {
+    p.innerText = inp.value;
+});
+// The code is already complete and functional. No additional code is needed.
