f68c664450ed2f23e3530ac1f8bee0bb2a47f9fb
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-12-14 23:43:10 +0530

    Refactor App component to enhance button functionality and add new Demo component

--- React/demo/src/App.jsx
+++ React/demo/src/App.jsx
@@ -1,34 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Clicked {count} times</h1>
+      <button onClick={() => { setCount(count + 1) }}>increase</button>
+      <button onClick={() => { setCount(count - 1) }}>decrease</button>
+      <br />
+      <button onClick={()=>{setCount(count == 0)}}>Reset</button>
+    </div>
+
   )
 }
 
