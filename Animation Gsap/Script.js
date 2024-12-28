bd6bd72be17ace21d48505379b7bc4ddcf7c7045
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-12-28 23:29:03 +0530

    Refactor animation code: comment out GSAP animations, update CSS for body background color, and add content to h1::after

--- Animation Gsap/Script.js
+++ Animation Gsap/Script.js
@@ -1,20 +1,20 @@
-gsap.from("#box",{
-    x : 1000 ,
-    duration : 11,
-    rotate : 360,
-    scale : 0.5,
-    borderRadius : "50%",
-    ease : "elastic",
+// gsap.from("#box",{
+//     x : 1000 ,
+//     duration : 11,
+//     rotate : 360,
+//     scale : 0.5,
+//     borderRadius : "50%",
+//     ease : "elastic",
 
-})
-gsap.to("h1",{
-    duration : 2,
-    text : "Hello World",
-    color : "red",
-    fontSize : "50px",
-    backgroundColor : "black",
-    padding : "20px",
-    borderRadius : "50%",
-    ease : "elastic",
-    delay : 5,
-})
+// })
+// gsap.to("h1",{
+//     duration : 2,
+//     text : "Hello World",
+//     color : "red",
+//     fontSize : "50px",
+//     backgroundColor : "black",
+//     padding : "20px",
+//     borderRadius : "50%",
+//     ease : "elastic",
+//     delay : 5,
+// })
