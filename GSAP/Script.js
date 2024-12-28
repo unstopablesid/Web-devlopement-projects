7f0970a3024c4369fd8003b02a17e075a349a36c
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-12-28 23:49:04 +0530

    Update GSAP animation: adjust entry effect parameters for smoother animation and enable repeat with yoyo effect

--- GSAP/Script.js
+++ GSAP/Script.js
@@ -9,12 +9,14 @@
 // )
 gsap.from(
     "#Box",{
-        x: 500,
+        x: 100,
         duration : 10,
         rotate : 360,
         borderRadius : "50%",
         
         opacity: 0,
         stagger: 1,
+        repeat : -1,
+        yoyo : true,
     }
 )
