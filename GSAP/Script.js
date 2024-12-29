17e2ed3ad603620c1ebcd9285b57a3f3b6714518
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-12-29 23:50:16 +0530

    Refactor GSAP animations: update HTML structure for multiple boxes, modify animation targets, and enhance styling for a cleaner layout

--- GSAP/Script.js
+++ GSAP/Script.js
@@ -7,16 +7,40 @@
 //         ease : "elastic",
 //     }
 // )
-gsap.from(
-    "#Box",{
-        x: 100,
-        duration : 10,
-        rotate : 360,
-        borderRadius : "50%",
+// gsap.from(
+//     "#Box",{
+//         x: 100,
+//         duration : 10,
+//         rotate : 360,
+//         borderRadius : "50%",
         
-        opacity: 0,
-        stagger: 1,
-        repeat : -1,
-        yoyo : true,
+//         opacity: 0,
+//         stagger: 1,
+//         repeat : -1,
+//         yoyo : true,
+//     }
+// )
+gsap.to(
+    "#page1 #Box1",{
+        duration : 5,
+        rotate : 360,
+        borderRadius : "100%",
+        ease : "elastic",
     }
-)
+    )
+    gsap.to(
+        "#page2 #Box2",{
+            duration : 5,
+            rotate : 360,
+            borderRadius : "50%",
+            ease : "elastic",
+        }
+        )
+        gsap.to(
+            "#page3 #Box3",{
+                duration : 5,
+                rotate : 360,
+                borderRadius : "50%",
+                ease : "elastic",
+            }
+            )
