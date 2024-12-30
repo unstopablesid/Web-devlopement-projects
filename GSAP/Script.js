e368c76b723d6c47fec3df754555f5d113da7c03
Mayur Patil <121495069+unstopablesid@users.noreply.github.com>
2024-12-30 23:02:44 +0530

    Add scrollTrigger functionality to GSAP animations for page1 and page2

--- GSAP/Script.js
+++ GSAP/Script.js
@@ -1,58 +1,58 @@
+// // gsap.to(
+// //     "#Box",{
+// //         x : 500 ,
+// //         duration : 5,
+// //         rotate : 360,
+// //         borderRadius : "50%",
+// //         ease : "elastic",
+// //     }
+// // )
+// // gsap.from(
+// //     "#Box",{
+// //         x: 100,
+// //         duration : 10,
+// //         rotate : 360,
+// //         borderRadius : "50%",
+        
+// //         opacity: 0,
+// //         stagger: 1,
+// //         repeat : -1,
+// //         yoyo : true,
+// //     }
+// // )
 // gsap.to(
-//     "#Box",{
-//         x : 500 ,
+//     "#page1 #Box1",{
 //         duration : 5,
 //         rotate : 360,
 //         borderRadius : "50%",
-//         ease : "elastic",
+//         scrollTrigger : {
+//             trigger : "#page1",
+//             start : "top center",
+//             end : "bottom center",
+//             scrub : true,
+//             markers : true,
+//         },
 //     }
-// )
-// gsap.from(
-//     "#Box",{
-//         x: 100,
-//         duration : 10,
+//     )
+//     gsap.to(
+//         "#page2 #Box2",{
+//         scrollTrigger : {
+//             trigger : "#page2",
+//             start : "top center",
+//             end : "bottom center",
+//             scrub : true,
+//             markers : true,
+//         },
+//         duration : 5,
 //         rotate : 360,
 //         borderRadius : "50%",
-        
-//         opacity: 0,
-//         stagger: 1,
-//         repeat : -1,
-//         yoyo : true,
 //     }
-// )
-gsap.to(
-    "#page1 #Box1",{
-        duration : 5,
-        rotate : 360,
-        borderRadius : "50%",
-        scrollTrigger : {
-            trigger : "#page1",
-            start : "top center",
-            end : "bottom center",
-            scrub : true,
-            markers : true,
-        },
-    }
-    )
-    gsap.to(
-        "#page2 #Box2",{
-        scrollTrigger : {
-            trigger : "#page2",
-            start : "top center",
-            end : "bottom center",
-            scrub : true,
-            markers : true,
-        },
-        duration : 5,
-        rotate : 360,
-        borderRadius : "50%",
-    }
-        )
-gsap.to(
-            "#page3 #Box3",{
-                duration : 5,
-                rotate : 360,
-                borderRadius : "50%",
-                ease : "elastic",
-            }
-            )
+//         )
+// gsap.to(
+//             "#page3 #Box3",{
+//                 duration : 5,
+//                 rotate : 360,
+//                 borderRadius : "50%",
+//                 ease : "elastic",
+//             }
+//             )
