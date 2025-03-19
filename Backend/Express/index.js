fbc18cc7871bb4175540db8fe504388f20b97bbd
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-03-20 00:59:43 +0530

    Add nodemon dependency and implement basic routing for the Express server

--- Backend/Express/index.js
+++ Backend/Express/index.js
@@ -7,12 +7,28 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.use((req,res)=>{
-    console.log('Request received');
-    res.send({
+// app.use((req,res)=>{
+//     console.log('Request received');
+//     res.send({
 
-        name:"Mayur",
-        age:24,
-    });
+//         name:"Mayur",
+//         age:24,
+//     });
+// });
+
+app.get('/', (req, res) => {
+
+     res.send('ROOT');
 });
+app.get('/contact', (req, res) => {
 
+     res.send('contact');
+});
+app.get('/Faq', (req, res) => {
+
+     res.send('Faqs');
+});
+app.get("*", (req, res) => {
+
+    res.send('This is 404 page');
+});
