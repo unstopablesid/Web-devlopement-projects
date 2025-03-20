c4ed6ca0932a7f7b30b0350076872a074a626a7f
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-03-20 23:31:07 +0530

    Implement dynamic user and search routes in Express server

--- Backend/Express/index.js
+++ Backend/Express/index.js
@@ -28,7 +28,22 @@ app.get('/Faq', (req, res) => {
 
      res.send('Faqs');
 });
-app.get("*", (req, res) => {
+// app.get("*", (req, res) => {
 
-    res.send('This is 404 page');
+//     res.send('This page not available');
+// });
+
+
+app.get('/:User/:id', (req, res) => {
+     console.log(req.params);
+     let {User ,id} = req.params;
+     let code = `<h1>User is ${User} and id is ${id}</h1>`;
+     res.send(code);
+     // res.send("User is "+User+" and id is "+id);
+});
+
+app.get('/Search', (req, res) => {
+     console.log(req.query);
+     let {query} = req.query;
+     res.send("Search result for "+q);
 });
