e05ebe84c676a9a2c1cbe051c2f7ed56360b0d1c
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-03-21 22:22:15 +0530

    Add Instagram and Roll Dice pages with dynamic routing and data rendering

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -7,9 +7,19 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.set ('views', Path.join(__dirname, 'views'));
 
-app.set('view engine', 'ejs');
+
+app.get('/rolldice', (req, res) => {
+    res.render("rolldice.ejs");
+});
 app.get('/', (req, res) => {
-  res.render("home.ejs")
+    res.render("home.ejs");
+});
+
+
+app.get('/instagram/:username', (req, res) => {
+    let {username} = req.params;
+    const instaData = require('./data.json');
+    const data = instaData[username];
+    res.render("instagram.ejs",{data});
 });
