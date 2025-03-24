6118958d29055c7f40e967f86300531e4a3c1c2e
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-03-24 23:58:45 +0530

    Add UUID support for posts and implement detail view for individual posts

--- REST/index.js
+++ REST/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const { v4 : uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -18,18 +20,22 @@ app.listen(port,(req,res)=>{
 
 let posts = [
     {
+        id: uuidv4(),
         username: "Mayur",
         content: "I love coding",
     },
     {
+        id: uuidv4(),
         username: "Shradha",
-        content: "I an motherfucking millionire",
+        content: "I am a motherfucking millionaire",
     },
     {
+        id: uuidv4(),
         username: "Homelander",
         content: "I can do whatever I want",
     },
     {
+        id: uuidv4(),
         username: "Arpit",
         content: "I am going live today at 11:00 PM",
     }
@@ -42,12 +48,20 @@ app.get('/',(req , res) => {
 app.get('/posts',(req , res) => {
     res.render('index.ejs',{posts:posts});
 }); 
+//this renders a new route
 app.get('/posts/new',(req,res)=>{
 res.render('new.ejs');
 });
-
+//this creates a new post
 app.post('/posts',(req,res)=>{
     let {username,content} = req.body;
-    posts.push({username,content});
+    let id = uuidv4();
+    posts.push({id,username,content});
     res.redirect('/posts');
+});
+
+app.get('/posts/:id',(req,res)=>{
+    let {id} = req.params.id;
+    let post = posts.find((p) => p.username === id);
+    res.render('show.ejs',{post:posts[id]});
 });
