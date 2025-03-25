1e10c7bcc0258fa73e3838931cadc608358f9532
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-03-25 22:42:58 +0530

    Add method-override support for PATCH and DELETE requests; update edit form and index viewf

--- REST/index.js
+++ REST/index.js
@@ -4,10 +4,11 @@ const port = 3000;
 const path = require('path');
 const { v4 : uuidv4 } = require('uuid');
 uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-
+app.use(methodOverride('_method'));
 app.set("views engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
@@ -74,7 +75,7 @@ app.patch('/posts/:id',(req,res)=>{
     let post = posts.find((p) => id === p.id);
     post.content = newContent;
     console.log(post);
-    res.send('PATCH request received');
+    res.render("/posts");
 });
 
 app.delete('/posts/:id',(req,res)=>{
@@ -87,4 +88,9 @@ app.get('/posts/:id/edit',(req,res)=>{
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
     res.render('edit.ejs',{post});
+});
+app.delete('/posts/:id',(req,res)=>{
+    let {id} = req.params;
+    let posts = posts.filter((p) => id !== p.id);
+    res.redirect('/posts');
 });
