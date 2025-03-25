d34b3da7d15dce914bdbb8fc27e6876b302b84be
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-03-25 23:55:33 +0530

    Add user data generation using Faker.js; create initial SQL files

---
+++ SQL/index.js
@@ -0,0 +1,14 @@
+
+const { faker }   = require('@faker-js/faker');
+
+
+ let getRandomUser =() =>{
+    return {
+      Id: faker.string.uuid(),
+      username: faker.internet.username(), 
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+  }
+
+  console.log(getRandomUser());
