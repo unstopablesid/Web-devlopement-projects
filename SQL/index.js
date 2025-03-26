993275c43a10c9371dec028c7d4e51798437432e
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-03-26 22:41:36 +0530

    Refactor user data generation to insert multiple records into the database in a single query

--- SQL/index.js
+++ SQL/index.js
@@ -2,15 +2,23 @@
 const { faker }   = require('@faker-js/faker');
 const mysql = require('mysql2');
 
- let getRandomUser =() =>{
-    return {
-      Id: faker.string.uuid(),
-      username: faker.internet.username(), 
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-  }
 
+let q = "insert into temp (id INT , username , email, password ) values ?";
+
+let getRandomUser =() =>{
+  return [
+    faker.string.uuid(),
+    faker.internet.username(), 
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+}
+
+let data = [];
+for (let i = 0; i < 100; i++) {
+    data.push(getRandomUser());
+    // console.log(getRandomUser());
+} 
 // Create a connection to the database
 
   const connection =  mysql.createConnection({
@@ -19,13 +27,12 @@ const mysql = require('mysql2');
     database: 'sigma_app',
     password: 'MRsidharth@2004'
   });
-try{
-  connection.query("SHOW TABLES" , (err, results) => {
-    if (err) {
-      console.error(err);
-    }
 
-    console.log(results);
+
+  try{
+  connection.query(q ,[data], (err, results) => {
+    if (err) throw err;
+      console.error(results);
   });}
     catch(err){
         console.log(err);
