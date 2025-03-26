be59fd7be6d543851d549cb12b67e902e962d895
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-03-27 00:57:02 +0530

    Add EJS views for home, edit, and user pages; update package.json with new dependencies

--- SQL/index.js
+++ SQL/index.js
@@ -1,11 +1,76 @@
-
-const { faker }   = require('@faker-js/faker');
+const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require('express');
+
+const app = express();
+const path = require("path");
+const { count } = require('console');
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set("views" , path.join(__dirname, "/views"));
 
 
-let q = "insert into temp (id INT , username , email, password ) values ?";
 
-let getRandomUser =() =>{
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  let q = `SELECT count(*) FROM user`;
+  try{
+      connection.query(q , (err, results) => {
+        if (err) throw err;
+        console.log(results[0]["count(*)"]);
+        res.render("home.ejs",{ count } );
+    });
+    } catch(err) {
+      console.log(err);
+      res.render("There is somethiong wrong");
+    }
+    
+});
+// homepage
+app.get('/user', (req, res) => {
+    let q = `SELECT * FROM user`;
+    try{
+        connection.query(q , (err, users) => {
+          if (err) throw err;
+          res.render("user.ejs",{ users} );
+      });
+      } catch(err) {
+        console.log(err);
+        res.render("There is somethiong wrong");
+      }
+
+});
+
+app.get('/user/:id/edit', (req, res) => {
+ let id = req.params.id;
+ let q = `SELECT * FROM user WHERE id = '${id}'`;
+ try{
+    connection.query(q , (err, user) => {
+      if (err) throw err;
+      res.render("edit.ejs",{ user: user[0] } );
+  });
+  } catch(err) {
+    console.log(err);
+    res.render("There is somethiong wrong");
+  }
+ 
+});
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'sigma_app',
+  password: 'MRsidharth@2004'
+});
+
+let getRandomUser = () => {
   return [
     faker.string.uuid(),
     faker.internet.username(), 
@@ -14,27 +79,22 @@ let getRandomUser =() =>{
   ];
 }
 
-let data = [];
-for (let i = 0; i < 100; i++) {
-    data.push(getRandomUser());
-    // console.log(getRandomUser());
-} 
-// Create a connection to the database
 
-  const connection =  mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'sigma_app',
-    password: 'MRsidharth@2004'
-  });
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let data = [];
+
+// for (let i = 0; i < 100; i++) {
+//   data.push(getRandomUser());
+// }
 
 
-  try{
-  connection.query(q ,[data], (err, results) => {
-    if (err) throw err;
-      console.error(results);
-  });}
-    catch(err){
-        console.log(err);
-    }
-   connection.end();
+// try{
+//   connection.query(q , [data] , (err, results) => {
+//     if (err) throw err;
+//     console.log(results);
+// });
+// } catch(err) {
+//   console.log(err);
+// }
+
+// connection.end();
